c6dfd47e7cc5bf38b606f3966b84f6d82d18ef50
Ruth Vera <rvamore89@gmail.com>
2020-05-22 22:14:53 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -21,8 +21,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-  
-  return [index]
+  return[index]
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
